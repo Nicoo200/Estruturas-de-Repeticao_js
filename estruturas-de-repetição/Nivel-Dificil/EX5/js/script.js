1fe1008bc09692df2896bacebb7a1fa700b14fb5
Nicoo200 <nicollass049@gmail.com>
2025-02-24 09:19:14 -0300

    Exercicio 5 arrumado

--- estruturas-de-repetição/Nivel-Dificil/EX5/js/script.js
+++ estruturas-de-repetição/Nivel-Dificil/EX5/js/script.js
@@ -1,2 +1,22 @@
-let num = parseInt(prompt("Digite um numero"));
+function decimalParaBinario(numero) {
+    if (numero === 0) {
+      return "0";
+    }
+  
+    let binario = '';
+    
+    while (numero > 0) {
 
+        binario = (numero % 2) + binario;
+        numero = Math.floor(numero / 2);
+    }
+  
+    return binario;
+}
+  
+let numeroDecimal  = parseInt(prompt("Digite um numero: "));
+
+let numeroBinario = decimalParaBinario(numeroDecimal);
+  
+alert("O seu numero convertido para binario é:" + numeroBinario); 
+  
